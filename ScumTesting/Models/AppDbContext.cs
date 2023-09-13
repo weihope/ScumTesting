65421a570fe483e8a06596c466c8947b7d33cc08
weihope <126637288+weihope@users.noreply.github.com>
2023-09-13 10:46:16 +0800

    connect to database

---
+++ ScumTesting/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScumTesting.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; } = null!;
+
+    }
+}
--- ScumTesting/Program.cs
+++ ScumTesting/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ScumTesting.Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Connect SQL
+builder.Services.AddDbContext<AppDbContext>
+    (options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ScrumTesting")));
 
 var app = builder.Build();
 
